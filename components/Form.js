77882c415bd3432abca097295bc91db35a1dec5f
wilyos <wilyos99@gmail.com>
2023-03-22 16:34:36 -0500

    funcion de guardado y funcionamiento de los inputs y primer boton

--- components/Form.js
+++ components/Form.js
@@ -1,9 +1,56 @@
+import { useState } from 'react';
 import {Text, View, TouchableOpacity } from 'react-native';
-import { TextInput } from 'react-native-web';
+import {TextInput } from 'react-native-web';
 import {styles} from '../assets/styles/Style'
 
 
 function Form(){
+//definimos el objeto para introducir datos y las variables
+    const estudiantes = []
+
+    const [id,setid]= useState('')
+    const [names,setNames]= useState('')
+    const [course,setCourse]= useState('')
+    const [firstN,setFirstN]= useState('')
+    const [secondN,setSecondN]= useState('')
+    const [thirdN,setThirdN]= useState('')
+    const [finalN,setFinalN]= useState('')
+    const [observ,setObserv]= useState('')
+    
+    let Guardar =()=>{
+      let notaFinal = 0
+      let nota1 = parseFloat(firstN)
+      let nota2 = parseFloat(secondN)
+      let nota3 = parseFloat(thirdN)
+
+      if(id != '' && names != '' && course != '' && firstN != '' && secondN != '' && thirdN !=''){
+        if(nota1 && nota2 && nota3 < 5){
+
+          notaFinal = (nota1 * 0.3)+(nota2*0.35)+(nota3*0.35)
+          setFinalN(notaFinal)
+
+          if(notaFinal < 2){
+            setObserv("Reprueba")
+          }
+          if (notaFinal < 3) {
+            setObserv("Habilita")
+          } 
+          else {
+            setObserv("Aprueba")
+          } 
+        }
+        else{
+          alert("las notas deben ir entre 0 y 5")
+        }
+
+      }
+      else{
+        alert("Todos los datos deben ser ingresados")
+      }
+
+    }
+
+
     return(
         <View style={styles.boxform}>
         <View style={styles.fila}>
@@ -11,7 +58,7 @@ function Form(){
               <Text>Identificación:</Text>
             </View>
             <View style={styles.boxitems}>
-              <TextInput placeholder=''style={styles.textinput} />
+              <TextInput placeholder=''style={styles.textinput} onChangeText={id=>setid(id)} />
             </View>
         </View>
         <View style={styles.fila}>
@@ -19,7 +66,7 @@ function Form(){
               <Text>Nombres:</Text>
             </View>
             <View style={styles.boxitems}>
-              <TextInput placeholder=''style={styles.textinput} />
+              <TextInput placeholder=''style={styles.textinput} onChangeText={names=>setNames(names)}/>
             </View>
         </View>
         <View style={styles.fila}>
@@ -27,7 +74,7 @@ function Form(){
               <Text>Asignación:</Text>
             </View>
             <View style={styles.boxitems}>
-              <TextInput placeholder=''style={styles.textinput} />
+              <TextInput placeholder=''style={styles.textinput} onChangeText={course=>setCourse(course)}/>
             </View>
         </View>
         <View style={styles.fila}>
@@ -35,7 +82,7 @@ function Form(){
               <Text>Nota momento 1(30%):</Text>
             </View>
             <View style={styles.boxitems}>
-              <TextInput placeholder=''style={styles.textinput} />
+              <TextInput placeholder=''style={styles.textinput} onChangeText={firstN=>setFirstN(firstN)}/>
             </View>
         </View>
         <View style={styles.fila}>
@@ -43,14 +90,14 @@ function Form(){
               <Text>Nota momento 2(35%):</Text>
             </View>
             <View style={styles.boxitems}>
-              <TextInput placeholder=''style={styles.textinput} />
+              <TextInput placeholder=''style={styles.textinput} onChangeText={secondN=>setSecondN(secondN)} />
             </View>
         </View><View style={styles.fila}>
             <View style={styles.boxitems}>
               <Text>Nota momento 3(35%):</Text>
             </View>
             <View style={styles.boxitems}>
-              <TextInput placeholder=''style={styles.textinput} />
+              <TextInput placeholder=''style={styles.textinput} onChangeText={thirdN=>setThirdN(thirdN)}/>
             </View>
         </View>
         <View style={styles.fila}>
@@ -58,7 +105,7 @@ function Form(){
               <Text>Definitiva:</Text>
             </View>
             <View style={styles.boxitems}>
-              <TextInput placeholder=''style={styles.textinput} />
+              <TextInput placeholder=''style={styles.textinput} value={finalN} />
             </View>
         </View>
         <View style={styles.fila}>
@@ -66,12 +113,12 @@ function Form(){
               <Text>Observación:</Text>
             </View>
             <View style={styles.boxitems}>
-              <TextInput placeholder=''style={styles.textinput} />
+              <TextInput placeholder=''style={styles.textinput} value={observ} />
             </View>
         </View>
         <View style={styles.fila}>
             <View style={styles.boxitems}>
-                <TouchableOpacity style={[styles.Touchables]}>
+                <TouchableOpacity style={[styles.Touchables]} onPress={()=>Guardar()}>
                     <Text>Calcular/Guardar</Text>
                 </TouchableOpacity>
             </View>
